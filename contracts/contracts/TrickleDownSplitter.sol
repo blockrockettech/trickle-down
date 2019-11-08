e65c5e2895cd289f6f9f270fbb3f407a4083b854
Vincent de Almeida <vince0656@gmail.com>
2019-11-08 11:17:15 +0000

    Added withdraw function and set curators

--- contracts/contracts/TrickleDownSplitter.sol
+++ contracts/contracts/TrickleDownSplitter.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.5.8;
 
-contract TrickleDownSplitter {
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/roles/WhitelistedRole.sol";
 
+contract TrickleDownSplitter is WhitelistedRole {
+    using SafeMath for uint256;
+
+    address[] curators;
+
+    constructor() public {
+        super.addWhitelisted(msg.sender);
+    }
+
+    function setCurators(address[] calldata _curators) onlyWhitelisted external {
+        require(_curators.length > 0, "No addresses have been supplied");
+        curators = _curators;
+    }
+
+    function withdrawAllFunds() onlyWhitelisted external {
+        msg.sender.call.value(address(this).balance)("");
+    }
+
+    function _convertToAddressPayable(address toConvert) private pure returns (address payable) {
+        return address(uint160(toConvert));
+    }
 }
